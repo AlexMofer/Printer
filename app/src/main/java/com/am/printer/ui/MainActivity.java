33f14e3f0d1fa87711161f1d895f6e02733d1711
Alex <moferalex@gmail.com>
2021-05-14 11:00:52 +0800

    [update] 发布2.1.2版本

---
+++ app/src/main/java/com/am/printer/ui/MainActivity.java
@@ -0,0 +1,10 @@
+package com.am.printer.ui;
+
+import com.am.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    public MainActivity() {
+        super(R.layout.activity_main);
+    }
+}
